d8048b873b42e8fb43c69bb3b8724b86ba04aefa
Mathieu Leplatre <mathieu@mozilla.com>
2014-12-18 09:37:43 +0100

    Default 999 for error number

--- readinglist/exceptions.py
+++ readinglist/exceptions.py
@@ -1,12 +1,14 @@
 class InvalidUsage(Exception):
     status_code = 400
     description = "Bad Request"
+    error_number = 999
 
     def __init__(self, message, status_code=None,
                  error_number=None, description=None):
         super(InvalidUsage, self).__init__()
         self.message = message
-        self.error_number = error_number or 0
+        if error_number is not None:
+            self.error_number = error_number
         if status_code is not None:
             self.status_code = status_code
         if description is not None:
