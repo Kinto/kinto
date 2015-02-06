c09fe71817490f2df6a0aee11679745398203497
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-02-06 19:29:36 +0100

    Use DummyRequest.

--- readinglist/tests/support.py
+++ readinglist/tests/support.py
@@ -16,8 +16,8 @@ from readinglist.utils import random_bytes_hex
 class DummyRequest(mock.MagicMock):
     def __init__(self, *args, **kwargs):
         super(DummyRequest, self).__init__(*args, **kwargs)
+        self.registry = mock.MagicMock(settings={})
         self.GET = {}
-        self.registry.settings = {}
         self.headers = {}
         self.errors = cornice_errors.Errors()
         self.authenticated_userid = 'bob'
