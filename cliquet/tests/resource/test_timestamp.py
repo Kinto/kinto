edbd71d6997b6de8d6aae3ae12c3fc6384aca271
Alexis Métaireau <alexis@notmyidea.org>
2015-02-19 23:08:52 +0100

    Add back the timestamp test

---
+++ cliquet/tests/resource/test_timestamp.py
@@ -0,0 +1,14 @@
+from .support import unittest
+
+from cliquet.resource import TimeStamp
+
+import colander
+import mock
+
+
+class TimeStampTest(unittest.TestCase):
+    @mock.patch('cliquet.resource.msec_time')
+    def test_default_value_comes_from_timestamper(self, time_mocked):
+        time_mocked.return_value = 666
+        default = TimeStamp().deserialize(colander.null)
+        self.assertEqual(default, 666)
