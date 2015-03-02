69cf6c0e2f87f94a53f9d8252a711d84bbbb918c
Alexis Métaireau <alexis@notmyidea.org>
2015-03-02 15:41:14 +0100

    Use postgreSQL per default. fix #3

--- kinto/tests/support.py
+++ kinto/tests/support.py
@@ -20,7 +20,8 @@ class BaseWebTest(cliquet_support.BaseWebTest):
             'cliquet.project_name': 'cloud storage',
             'cliquet.project_docs': 'https://kinto.rtfd.org/',
             'cliquet.basic_auth_enabled': 'true',
-            'cliquet.storage_backend': 'cliquet.storage.redis',
+            'cliquet.storage_backend': 'cliquet.storage.postgresql',
+            'cliquet.storage_url': 'postgres:postgres@localhost/postgres',
             'cliquet.session_backend': 'cliquet.session.redis',
             'fxa-oauth.client_id': '89513028159972bc',
             'fxa-oauth.client_secret': '9aced230585cc0aa2932e2eb871c9a3a7d6458'
