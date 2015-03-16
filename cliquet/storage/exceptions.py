d97fb9ab1283547d3edb3a588a02f0c7bfe2e74b
Mathieu Leplatre <mathieu@mozilla.com>
2015-03-16 22:26:35 +0100

    Fix UnicityError representation

--- cliquet/storage/exceptions.py
+++ cliquet/storage/exceptions.py
@@ -34,5 +34,5 @@ class UnicityError(IntegrityError):
     def __init__(self, field, record, *args, **kwargs):
         self.field = field
         self.record = record
-        message = "{0} is not unique: {1}".format(field, record)
+        self.msg = "{0} is not unique: {1}".format(field, record)
         super(UnicityError, self).__init__(*args, **kwargs)
