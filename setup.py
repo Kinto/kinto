cd1c96c41aaca0ce7419ac2557d8895f78719a11
Mathieu Leplatre <mathieu@mozilla.com>
2015-03-19 15:32:04 +0100

    Add Apache Licese

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
+with open(os.path.join(here, 'CHANGELOG.rst')) as f:
+    CHANGELOG = f.read()
+
+
 REQUIREMENTS = [
     'colander',
     'cornice',
@@ -23,11 +27,13 @@ ENTRY_POINTS = {
 setup(name='kinto',
       version='0.1.dev0',
       description='kinto',
-      long_description=README,
+      long_description=README + "\n\n" + CHANGELOG,
+      license='Apache License (2.0)',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "License :: OSI Approved :: Apache Software License"
       ],
       keywords="web services",
       author='Mozilla Services',
