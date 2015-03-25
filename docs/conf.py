271190ed7e76aa75325a5d3852f99cbaed43a4d1
Mathieu Leplatre <mathieu@mozilla.com>
2015-03-25 18:00:54 +0100

    Update version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ copyright = u'2015, Mozilla Services — Da French Team'
 # The short X.Y version.
 version = '1.4'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.0'
+release = '1.4.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
