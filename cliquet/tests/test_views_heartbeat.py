334b662139be7a2aa2a910e3c87ea90884f644a2
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-02 11:35:45 +0200

    Speed-up test with FxA heartbeat bypass

--- cliquet/tests/test_views_heartbeat.py
+++ cliquet/tests/test_views_heartbeat.py
@@ -6,26 +6,31 @@ from .support import BaseWebTest, unittest
 
 class HeartBeatViewTest(BaseWebTest, unittest.TestCase):
 
-    def test_returns_database_true_if_ok(self):
+    @mock.patch('cliquet.views.heartbeat.fxa_ping')
+    def test_returns_database_true_if_ok(self, *mocked):
         response = self.app.get('/__heartbeat__')
         self.assertEqual(response.json['database'], True)
 
-    def test_returns_cache_true_if_ok(self):
+    @mock.patch('cliquet.views.heartbeat.fxa_ping')
+    def test_returns_cache_true_if_ok(self, *mocked):
         response = self.app.get('/__heartbeat__')
         self.assertEqual(response.json['cache'], True)
 
-    def test_returns_oauth_true_if_ok(self):
+    @mock.patch('cliquet.views.heartbeat.fxa_ping', return_value=True)
+    def test_returns_oauth_true_if_ok(self, *mocked):
         response = self.app.get('/__heartbeat__')
         self.assertEqual(response.json['oauth'], True)
 
-    @mock.patch('cliquet.storage.memory.Memory.ping')
+    @mock.patch('cliquet.views.heartbeat.fxa_ping')
     @mock.patch('cliquet.storage.redis.Redis.ping')
+    @mock.patch('cliquet.storage.memory.Memory.ping')
     def test_returns_database_false_if_ko(self, *mocked):
         for mock_instance in mocked:
             mock_instance.return_value = False
         response = self.app.get('/__heartbeat__', status=503)
         self.assertEqual(response.json['database'], False)
 
+    @mock.patch('cliquet.views.heartbeat.fxa_ping')
     @mock.patch('cliquet.cache.redis.Redis.ping')
     def test_returns_cache_false_if_ko(self, *mocked):
         for mock_instance in mocked:
