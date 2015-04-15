79b4897ddeca3d8ffb982fd3bcd3bf2accc9e4c4
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-15 12:27:29 +0200

    Revert "Use bidirectionnal conversion for epoch/timestamp"
    This reverts commit 507f720c919b4af4ef1d78f5bd928660c8b2ca96.

--- cliquet/storage/postgresql/migrations/migration_004_005.sql
+++ cliquet/storage/postgresql/migrations/migration_004_005.sql
@@ -1,15 +1,6 @@
-CREATE OR REPLACE FUNCTION from_epoch(ts BIGINT) RETURNS TIMESTAMP AS $$
-BEGIN
-    RETURN to_timestamp(ts / 1000.0) AT TIME ZONE 'UTC';
-END;
-$$ LANGUAGE plpgsql
-IMMUTABLE;
-
-
 DROP INDEX IF EXISTS idx_records_user_id;
 DROP INDEX IF EXISTS idx_records_resource_name;
 DROP INDEX IF EXISTS idx_records_last_modified;
-DROP INDEX IF EXISTS idx_records_last_modified_epoch;
 DROP INDEX IF EXISTS idx_records_id;
 
 ALTER TABLE records
@@ -20,7 +11,6 @@ DROP INDEX IF EXISTS idx_deleted_id;
 DROP INDEX IF EXISTS idx_deleted_user_id;
 DROP INDEX IF EXISTS idx_deleted_resource_name;
 DROP INDEX IF EXISTS idx_deleted_last_modified;
-DROP INDEX IF EXISTS idx_deleted_last_modified_epoch;
 
 ALTER TABLE deleted
     ADD PRIMARY KEY (id, user_id, resource_name);
