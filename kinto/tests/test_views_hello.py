f431cb3120beb17f4db03f46e35c817c314c34d5
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-24 17:17:11 +0200

    Import tests from Reading List

--- kinto/tests/test_views_hello.py
+++ kinto/tests/test_views_hello.py
@@ -1,7 +1,4 @@
-import mock
-from webtest.app import TestRequest
-
-from kinto import __version__ as VERSION, API_VERSION
+from kinto import __version__ as VERSION
 
 from .support import BaseWebTest, unittest
 
@@ -11,7 +8,7 @@ class HelloViewTest(BaseWebTest, unittest.TestCase):
     def test_returns_info_about_url_and_version(self):
         response = self.app.get('/')
         self.assertEqual(response.json['version'], VERSION)
-        self.assertEqual(response.json['url'], 'http://localhost')
+        self.assertEqual(response.json['url'], 'http://localhost/v0/')
         self.assertEqual(response.json['hello'], 'cloud storage')
         self.assertEqual(response.json['documentation'],
                          'https://kinto.rtfd.org/')
