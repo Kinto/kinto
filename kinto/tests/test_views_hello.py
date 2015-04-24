de76d7c28f2d9853c9cfb9b209f17b668c09f203
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-24 17:17:38 +0200

    Get rid of useless tests

--- kinto/tests/test_views_hello.py
+++ kinto/tests/test_views_hello.py
@@ -12,36 +12,3 @@ class HelloViewTest(BaseWebTest, unittest.TestCase):
         self.assertEqual(response.json['hello'], 'cloud storage')
         self.assertEqual(response.json['documentation'],
                          'https://kinto.rtfd.org/')
-
-    def test_do_not_returns_eos_if_empty_in_settings(self):
-        response = self.app.get('/')
-        self.assertNotIn('eos', response.json)
-
-    def test_returns_eos_if_not_empty_in_settings(self):
-        eos = '2069-02-21'
-        with mock.patch.dict(
-                self.app.app.registry.settings,
-                [('cliquet.eos', eos)]):
-            response = self.app.get('/')
-            self.assertEqual(response.json['eos'], eos)
-
-    def test_redirect_to_version(self):
-        # We don't want the prefix to be automatically added for this test.
-        original_request_class = self.app.RequestClass
-
-        try:
-            self.app.RequestClass = TestRequest  # Standard RequestClass.
-
-            # GET on the hello view.
-            response = self.app.get('/')
-            self.assertEqual(response.status_int, 307)
-            self.assertEqual(response.location,
-                             'http://localhost/%s/' % API_VERSION)
-
-            # GET on the fields view.
-            response = self.app.get('/articles')
-            self.assertEqual(response.status_int, 307)
-            self.assertEqual(response.location,
-                             'http://localhost/%s/articles' % API_VERSION)
-        finally:
-            self.app.RequestClass = original_request_class
