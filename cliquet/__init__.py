e49cd5cf44dcd8ada4def307b4f1f7e7862fac6a
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-27 13:37:38 +0200

    Unfold code

--- cliquet/__init__.py
+++ cliquet/__init__.py
@@ -144,8 +144,7 @@ def handle_api_redirection(config):
     config.route_prefix = None
 
     config.add_route(name='redirect_to_version',
-                     pattern='/{path:(?!%s).*}' %
-                     route_prefix)
+                     pattern='/{path:(?!%s).*}' % route_prefix)
 
     config.add_view(view=_redirect_to_version_view,
                     route_name='redirect_to_version',
