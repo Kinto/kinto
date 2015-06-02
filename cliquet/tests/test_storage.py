f5a75af88ac5fece209b86a41f6ff01fa111aaa3
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-02 17:47:03 +0200

    Move Cloud Storage to Kinto codebase

--- cliquet/tests/test_storage.py
+++ cliquet/tests/test_storage.py
@@ -3,12 +3,11 @@ import time
 import mock
 import psycopg2
 import redis
-import requests
 
 from cliquet import utils
 from cliquet.storage import (
     exceptions, Filter, generators, memory,
-    redis as redisbackend, postgresql, cloud_storage,
+    redis as redisbackend, postgresql,
     Sort, StorageBase
 )
 
@@ -894,43 +893,3 @@ class PostgresqlStorageTest(StorageTest, unittest.TestCase):
         with mock.patch('cliquet.storage.postgresql.warnings.warn') as mocked:
             self.backend.load_from_config(self._get_config(settings=settings))
             mocked.assert_any_call(msg)
-
-
-class CloudStorageTest(StorageTest, unittest.TestCase):
-    backend = cloud_storage
-    settings = {
-        'cliquet.storage_url': 'http://localhost:8888'
-    }
-
-    def setUp(self):
-        super(CloudStorageTest, self).setUp()
-        self.client_error_patcher = mock.patch.object(
-            self.storage._client,
-            'request',
-            side_effect=requests.ConnectionError)
-
-    def test_raises_backenderror_when_remote_returns_500(self):
-        with mock.patch.object(self.storage._client, 'request') as mocked:
-            error_response = requests.models.Response()
-            error_response.status_code = 500
-            error_response._content_consumed = True
-            error_response._content = u'Internal Error'.encode('utf8')
-            mocked.return_value = error_response
-            self.assertRaises(exceptions.BackendError,
-                              self.storage.get_all,
-                              **self.storage_kw)
-
-    def test_returns_backend_error_when_batch_has_error_in_responses(self):
-        batch_responses = {
-            'responses': [
-                {'status': 400}
-            ]
-        }
-        rules = [[Filter('number', 1, utils.COMPARISON.GT)]]
-        with mock.patch.object(self.storage._client, 'post') as mocked:
-            response_body = mock.Mock(return_value=batch_responses)
-            mocked.return_value = mock.MagicMock(json=response_body)
-            self.assertRaises(exceptions.BackendError,
-                              self.storage.get_all,
-                              limit=5, pagination_rules=rules,
-                              **self.storage_kw)
