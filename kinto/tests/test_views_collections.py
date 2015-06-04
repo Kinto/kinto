f4bdf866eceb0a00c153c501bcea9eac2c05b44f
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-04 16:02:58 +0200

    Introduce bucket in collections URLs

--- kinto/tests/test_views_collections.py
+++ kinto/tests/test_views_collections.py
@@ -6,52 +6,52 @@ MINIMALIST_ITEM = dict(name="Hulled Barley",
 
 
 class CollectionViewTest(BaseWebTest, unittest.TestCase):
+
+    collection_url = '/buckets/beers/collections/barley/records'
+    record_url = '/buckets/beers/collections/barley/records/%s'
+
     def test_empty_collection_returns_an_empty_list(self):
-        response = self.app.get('/collections/barley/records',
-                                headers=self.headers)
+        response = self.app.get(self.collection_url, headers=self.headers)
         self.assertEqual(response.json['items'], [])
 
     def test_individual_collections_can_be_deleted(self):
-        self.app.post('/collections/barley/records',
-                      headers=self.headers)
-
-        self.app.delete('/collections/barley/records',
-                        headers=self.headers)
+        self.app.post(self.collection_url, headers=self.headers)
+        self.app.delete(self.collection_url, headers=self.headers)
 
     def test_items_can_be_added_to_collections(self):
-        response = self.app.post_json('/collections/barley/records',
+        response = self.app.post_json(self.collection_url,
                                       MINIMALIST_ITEM,
                                       headers=self.headers)
         _id = response.json.get('id')
         self.assertIsNotNone(_id)
-        response = self.app.get('/collections/barley/records/%s' % _id,
-                                headers=self.headers)
+        response = self.app.get(self.record_url % _id, headers=self.headers)
 
         item = response.json
         del item['id']
         del item['last_modified']
         self.assertEquals(item, MINIMALIST_ITEM)
 
-    def test_collections_are_user_bound(self):
+    def test_collections_are_bound_by_bucket(self):
         # Add items in the collections.
-        response = self.app.post_json('/collections/barley/records',
+        response = self.app.post_json(self.collection_url,
                                       MINIMALIST_ITEM,
                                       headers=self.headers)
-        self.app.get('/collections/barley/records/%s' % response.json['id'],
+        collection_url = self.record_url % response.json['id']
+        self.app.get(collection_url.replace('beers', 'sodas'),
                      headers=get_user_headers("alice"), status=404)
 
     def test_collection_items_can_be_accessed_by_id(self):
-        response = self.app.post_json('/collections/barley/records',
+        response = self.app.post_json(self.collection_url,
                                       MINIMALIST_ITEM,
                                       headers=self.headers)
-        self.app.get('/collections/barley/records/%s' % response.json['id'],
+        self.app.get(self.record_url % response.json['id'],
                      headers=self.headers)
 
     def test_collection_items_can_be_filtered_on_any_field(self):
-        self.app.post_json('/collections/barley/records',
+        self.app.post_json(self.collection_url,
                            MINIMALIST_ITEM,
                            headers=self.headers)
-        response = self.app.get('/collections/barley/records?unknown=1',
+        response = self.app.get(self.collection_url + '?unknown=1',
                                 headers=self.headers)
         self.assertEqual(len(response.json['items']), 0)
 
@@ -59,11 +59,11 @@ class CollectionViewTest(BaseWebTest, unittest.TestCase):
         for i in range(3):
             record = MINIMALIST_ITEM.copy()
             record['name'] = 'Stout %s' % i
-            self.app.post_json('/collections/barley/records',
+            self.app.post_json(self.collection_url,
                                record,
                                headers=self.headers)
 
-        response = self.app.get('/collections/barley/records?_sort=-name',
+        response = self.app.get(self.collection_url + '?_sort=-name',
                                 headers=self.headers)
         names = [i['name'] for i in response.json['items']]
         self.assertEqual(names, ['Stout 2', 'Stout 1', 'Stout 0'])
--- kinto/views/collection.py
+++ kinto/views/collection.py
@@ -7,9 +7,11 @@ class RecordSchema(schema.ResourceSchema):
 
 
 @resource.register(
-    record_path="/collections/{{collection_id}}/records/{{id}}",
-    collection_path="/collections/{{collection_id}}/records",
-    name="collection")
+    name="collection",
+    record_path=("/buckets/{{bucket_id}}"
+                 "/collections/{{collection_id}}/records/{{id}}"),
+    collection_path=("/buckets/{{bucket_id}}"
+                     "/collections/{{collection_id}}/records"))
 class Collection(resource.BaseResource):
 
     mapping = RecordSchema()
@@ -17,6 +19,7 @@ class Collection(resource.BaseResource):
     def __init__(self, *args, **kwargs):
         super(Collection, self).__init__(*args, **kwargs)
         self.collection.collection_id = self.request.matchdict['collection_id']
+        self.collection.parent_id = self.request.matchdict['bucket_id']
 
     def is_known_field(self, field_name):
         """Without schema, any field is considered as known."""
