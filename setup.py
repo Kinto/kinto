8d904ea9bd8c9212d3ddd3c6f2943a626b466a64
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-04 16:21:59 +0200

    Tag intermediary version 2.0.dev0 (not released)

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ ENTRY_POINTS = {
 
 
 setup(name='cliquet',
-      version='1.8.1.dev0',
+      version='2.0.dev0',
       description='cliquet',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
