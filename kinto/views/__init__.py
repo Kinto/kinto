06104b04b282adfce5caccaf8004534b696f7c11
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-05 11:04:39 +0200

    Python3 compat

--- kinto/views/__init__.py
+++ kinto/views/__init__.py
@@ -6,6 +6,10 @@ from cliquet.storage import generators
 
 class NameGenerator(generators.Generator):
     def __call__(self):
-        alphabet = string.lowercase + '-'
+        try:
+            ascii_letters = string.ascii_letters
+        except ImportError:
+            ascii_letters = string.letters  # NOQA
+        alphabet = ascii_letters + string.digits + '-'
         letters = [random.choice(alphabet) for x in range(8)]
         return ''.join(letters)
