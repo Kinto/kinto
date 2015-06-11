a0c335a95e9f793e839e2bd6787f2e0f18829dc2
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-06-11 14:21:20 +0200

    Update kinto with last cliquet + Add tests for groups.

--- kinto/tests/support.py
+++ kinto/tests/support.py
@@ -13,6 +13,8 @@ MINIMALIST_COLLECTION = dict()
 MINIMALIST_GROUP = dict(members=['fxa:user'])
 MINIMALIST_RECORD = dict(name="Hulled Barley",
                          type="Whole Grain")
+USER_PRINCIPAL = 'basicauth_967e5a9f60cbe491bd8a695d8d0515caba27c' \
+                 '5c4f404a11ff86e14d3256d88b7'
 
 
 class BaseWebTest(object):
@@ -23,7 +25,10 @@ class BaseWebTest(object):
     def __init__(self, *args, **kwargs):
         super(BaseWebTest, self).__init__(*args, **kwargs)
         self.app.RequestClass = cliquet_support.get_request_class(prefix="v1")
+        self.principal = USER_PRINCIPAL
         self.storage = self.app.app.registry.storage
+        self.permission = self.app.app.registry.permission
+        self.permission.flush()
         self.storage.initialize_schema()
         self.headers = {
             'Content-Type': 'application/json',
@@ -33,6 +38,11 @@ class BaseWebTest(object):
     def tearDown(self):
         super(BaseWebTest, self).tearDown()
         self.storage.flush()
+        self.permission.flush()
+
+    def add_permission(self, object_id, permission):
+        self.permission.add_principal_to_ace(
+            object_id, permission, self.principal)
 
 
 def get_user_headers(user):
