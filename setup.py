497ee2392fc51f7206b141d201b83437545b3a46
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-16 10:44:35 +0200

    Bump to 2.0.dev4

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ ENTRY_POINTS = {
 
 
 setup(name='cliquet',
-      version='2.0.dev1',
+      version='2.0.dev4',
       description='cliquet',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
