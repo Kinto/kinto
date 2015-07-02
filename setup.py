eba367cf43e4e180cfd2826d38bfccee629effda
Mathieu Leplatre <mathieu@mozilla.com>
2015-07-02 13:47:51 +0200

    Back to development: 2.2.1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ ENTRY_POINTS = {
 
 
 setup(name='cliquet',
-      version='2.2.0',
+      version='2.2.1.dev0',
       description='cliquet',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
