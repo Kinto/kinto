d080e08ba4d97873c46d384e7d42fad76c5cc764
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-09-08 19:44:36 +0200

    Preparing release 2.6.1

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ ENTRY_POINTS = {
 
 
 setup(name='cliquet',
-      version='2.7.0.dev0',
+      version='2.6.1',
       description='Micro service API toolkit',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
