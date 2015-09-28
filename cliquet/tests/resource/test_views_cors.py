e8a2e215af32a3abb807d1337dbc136d226d2034
Mathieu Leplatre <mathieu@mozilla.com>
2015-09-28 18:51:58 +0200

    Add missing tests of app initialization

--- cliquet/tests/resource/test_views_cors.py
+++ cliquet/tests/resource/test_views_cors.py
@@ -181,7 +181,7 @@ class CORSExposeHeadersTest(BaseWebTest, unittest.TestCase):
             'Alert', 'Backoff', 'Retry-After', 'Content-Length'], status=404)
 
 
-class CORSMaxAgeTest(BaseWebTest):
+class CORSMaxAgeTest(BaseWebTest, unittest.TestCase):
     def setUp(self):
         super(CORSMaxAgeTest, self).setUp()
         self.headers.update({
@@ -190,5 +190,16 @@ class CORSMaxAgeTest(BaseWebTest):
         })
 
     def test_cors_max_age_is_3600_seconds_by_default(self):
-        resp = self.app.options('/', headers=self.headers)
+        app = self.get_test_app()
+        resp = app.options('/', headers=self.headers)
         self.assertEqual(int(resp.headers['Access-Control-Max-Age']), 3600)
+
+    def test_cors_max_age_can_be_specified_in_settings(self):
+        app = self.get_test_app({'cliquet.cors_max_age': '42'})
+        resp = app.options('/', headers=self.headers)
+        self.assertEqual(int(resp.headers['Access-Control-Max-Age']), 42)
+
+    def test_cors_max_age_is_disabled_if_unset(self):
+        app = self.get_test_app({'cliquet.cors_max_age': ''})
+        resp = app.options('/', headers=self.headers)
+        self.assertNotIn('Access-Control-Max-Age', resp.headers)
