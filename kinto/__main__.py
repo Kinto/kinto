c507ce9ca479804f08b97e0262a14d8e1b51ea8e
AW <sofia.utsch@gmail.com>
2015-10-26 23:48:28 -0200

    kinto migrate and start commands

--- kinto/__main__.py
+++ kinto/__main__.py
@@ -1,47 +1,49 @@
 import argparse
 import sys
-import textwrap
-import warnings
 import subprocess
-import cliquet
+
 from pyramid.paster import bootstrap
 
+
 def kinto_migrate(env):
-	registry = env['registry']
+        registry = env['registry']
+        for backend in ('cache', 'storage', 'permission'):
+                if hasattr(registry, backend):
+                        getattr(registry, backend).initialize_schema()
 
-	for backend in ('cache','storage','permission'):
-		if hasattr(registry, backend):
-			getattr(registry, backend).initialize_schema()
 
-def main (args=None):
-	"""The main routine."""
-	if args is None:
-		args = sys.argv[1:]
-	
-	parser = argparse.ArgumentParser(description="Kinto administration commands")
-	subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands',help='init/start/migrate')
-	
-	parser_init = subparsers.add_parser('init') 
-	parser_init.set_defaults(which='init') 
+def main(args=None):
+        """The main routine."""
+        if args is None:
+                args = sys.argv[1:]
 
-	parser_migrate = subparsers.add_parser('migrate') 
-	parser_migrate.set_defaults(which='migrate') 
+        parser = argparse.ArgumentParser(description="Kinto commands")
+        subparsers = parser.add_subparsers(title='subcommands',
+                                           description='valid subcommands',
+                                           help='start/migrate')
 
-	parser_start = subparsers.add_parser('start') 
-	parser_start.set_defaults(which='start') 
+        parser_init = subparsers.add_parser('init')
+        parser_init.set_defaults(which='init')
 
-	args = vars(parser.parse_args())
+        parser_migrate = subparsers.add_parser('migrate')
+        parser_migrate.set_defaults(which='migrate')
 
-	if args['which'] =='init':
-	   	#Still needs to setup the configuration options
-		pass
-	elif args['which'] =='migrate':
-	   	env = bootstrap('config/kinto.ini')
-	  	kinto_migrate(env)
-	   	print("running migrations")
-	elif args['which'] =='start':
-	   	subprocess.call(["python","-m","pyramid.scripts.pserve","config/kinto.ini","--reload"])		
+        parser_start = subparsers.add_parser('start')
+        parser_start.set_defaults(which='start')
+
+        args = vars(parser.parse_args())
+
+        if args['which'] == 'init':
+                """Still needs to setup the configuration options"""
+                pass
+        elif args['which'] == 'migrate':
+                env = bootstrap('config/kinto.ini')
+                kinto_migrate(env)
+                print("running migrations")
+        elif args['which'] == 'start':
+                subprocess.call(["python", "-m", "pyramid.scripts.pserve",
+                                "config/kinto.ini", "--reload"])
 
 
 if __name__ == "__main__":
-	main()
+        main()
