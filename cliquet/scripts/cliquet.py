dce6c37fc5301c346a6ed0d355df76a6ec717df2
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-10-29 14:30:00 +0100

    Improve the migration command.

--- cliquet/scripts/cliquet.py
+++ cliquet/scripts/cliquet.py
@@ -17,15 +17,17 @@ def deprecated_init(env):
 def init_schema(env):
     registry = env['registry']
     settings = registry.settings
-
-    if settings.get('readonly', False):
-        message = 'Cannot migrate the database while in readonly mode.'
-        print(message, file=sys.stderr)
-        sys.exit(1)
+    readonly_backends = ('storage', 'permission')
+    readonly_mode = settings.get('readonly', False)
 
     for backend in ('cache', 'storage', 'permission'):
         if hasattr(registry, backend):
-            getattr(registry, backend).initialize_schema()
+            if readonly_mode and backend in readonly_backends:
+                message = ('Cannot migrate the %s backend while '
+                           'in readonly mode.' % backend)
+                warnings.warn(message, Warning)
+            else:
+                getattr(registry, backend).initialize_schema()
 
 
 def main():
