74cf04e2e32d7c1c66bce9bf3a3f3ba59c20ceca
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-11-02 14:35:43 +0100

    @leplatrem review.

--- kinto/__main__.py
+++ kinto/__main__.py
@@ -4,43 +4,47 @@ from cliquet.scripts import cliquet
 from pyramid.scripts import pserve
 from pyramid.paster import bootstrap
 
+CONFIG_FILE = 'config/kinto.ini'
+
 
 def main(args=None):
-        """The main routine."""
-        if args is None:
-                args = sys.argv[1:]
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
 
-        parser = argparse.ArgumentParser(description="Kinto commands")
-        subparsers = parser.add_subparsers(title='subcommands',
-                                           description='valid subcommands',
-                                           help='init/start/migrate')
+    parser = argparse.ArgumentParser(description="Kinto commands")
+    parser.add_argument('--ini',
+                        help='Application configuration file',
+                        dest='ini_file',
+                        required=False,
+                        default=CONFIG_FILE)
 
-        parser_init = subparsers.add_parser('init')
-        parser_init.set_defaults(which='init')
-        parser_init.add_argument('--config_file', required=False,
-                                 help='Config file may be passed as argument')
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       description='valid subcommands',
+                                       help='init/start/migrate')
 
-        parser_migrate = subparsers.add_parser('migrate')
-        parser_migrate.set_defaults(which='migrate')
+    parser_init = subparsers.add_parser('init')
+    parser_init.set_defaults(which='init')
 
-        parser_start = subparsers.add_parser('start')
-        parser_start.set_defaults(which='start')
+    parser_migrate = subparsers.add_parser('migrate')
+    parser_migrate.set_defaults(which='migrate')
 
-        args = vars(parser.parse_args())
+    parser_start = subparsers.add_parser('start')
+    parser_start.set_defaults(which='start')
 
-        if args['which'] == 'init':
-                if(args['config_file'] is None):
-                    env = bootstrap('config/kinto.ini')
-                else:
-                    config_file = format(args['config_file'])
-                    env = bootstrap(config_file)
-        elif args['which'] == 'migrate':
-                env = bootstrap('config/kinto.ini')
-                cliquet.init_schema(env)
-        elif args['which'] == 'start':
-                pserve_argv = ['pserve', 'config/kinto.ini', '--reload']
-                pserve.main(pserve_argv)
+    args = vars(parser.parse_args())
+    config_file = args['ini_file']
+    env = bootstrap(config_file)
+
+    if args['which'] == 'init':
+        # Not implemented yet
+        pass
+    elif args['which'] == 'migrate':
+        cliquet.init_schema(env)
+    elif args['which'] == 'start':
+        pserve_argv = ['pserve', config_file, '--reload']
+        pserve.main(pserve_argv)
 
 
 if __name__ == "__main__":
-        main()
+    main()
