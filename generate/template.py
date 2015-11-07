7217f778144073d0037ebb498d427740538d876f
oak11 <oaks9599@gmail.com>
2015-11-07 11:19:23 +0530

    prompt for backend added

--- generate/template.py
+++ generate/template.py
@@ -9,6 +9,17 @@ def render_template(template, destination, **kwargs):
         with open(destination, 'w+') as output:
             output.write(rendered)
 
+backend = raw_input("Which backend to use ? (1 - in-memory, 2 - postgresql, 3 - redis) :")
+if backend == '':
+    backend = 2;     #default
 
-render_template("kinto.tpl", "kinto.ini", secret=binascii.b2a_hex(os.urandom(8)))
-
+if backend == 2:
+    render_template("kinto.tpl", "kinto.ini",
+                    secret=binascii.b2a_hex(os.urandom(32)),
+                    storage_backend = "cliquet.storage.postgresql",
+                    storage_url = "postgres://postgres:postgres@localhost/postgres",
+                    cache_backend = "cliquet.cache.postgresql",
+                    cache_url ="postgres://postgres:postgres@localhost/postgres",
+                    permission_backend = "cliquet.permission.postgresql",
+                    permission_url = "postgres://postgres:postgres@localhost/postgres"
+                    )
