8f180a3e210c032686441e421c4a4b639296ce1d
Mathieu Leplatre <mathieu@mozilla.com>
2015-11-09 11:18:02 +0100

    @leplatrem review

--- cliquet/views/hello.py
+++ cliquet/views/hello.py
@@ -1,6 +1,6 @@
 from pyramid.security import NO_PERMISSION_REQUIRED
 
-from cliquet import Service, API_VERSION
+from cliquet import Service, PROTOCOL_VERSION
 
 hello = Service(name="hello", path='/', description="Welcome")
 
@@ -13,7 +13,7 @@ def get_hello(request):
     data = dict(
         hello=project_name,
         version=settings['project_version'],
-        protocol_version=API_VERSION,
+        protocol_version=PROTOCOL_VERSION,
         url=request.route_url(hello.name),
         documentation=settings['project_docs']
     )
