a78b8920d496b0b660461b7405b706fbaadd4009
Mathieu Leplatre <mathieu@mozilla.com>
2015-11-18 16:02:21 +0100

    Add test for loading plugins (#584, #578)
    As suggested by @Natim

---
+++ cliquet/tests/testplugin/__init__.py
@@ -0,0 +1,17 @@
+from cliquet import Service
+from cliquet.authorization import RouteFactory
+
+
+attachment = Service(name='attachment',
+                     description='Attach file to record',
+                     path='/attachment',
+                     factory=RouteFactory)
+
+
+@attachment.post(permission='attach')
+def attachment_post(request):
+    return {"ok": True}
+
+
+def includeme(config):
+    config.add_cornice_service(attachment)
