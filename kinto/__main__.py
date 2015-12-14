980bdc92910663ab409a31d678f7ca556969e494
oak11 <oaks9599@gmail.com>
2015-12-15 01:27:29 +0530

    bug fix- kinto init when kinto.ini does not exist

--- kinto/__main__.py
+++ kinto/__main__.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
 import argparse
+import os
 import sys
+from six.moves import input
 from cliquet.scripts import cliquet
 from pyramid.scripts import pserve
 from pyramid.paster import bootstrap
-from config import init
+
+from kinto.config import init
 
 CONFIG_FILE = 'kinto/config/kinto.ini'
 
@@ -19,6 +23,11 @@ def main(args=None):
                         dest='ini_file',
                         required=False,
                         default=CONFIG_FILE)
+    parser.add_argument('--backend',
+                        help='Specify backend',
+                        dest='backend',
+                        required=False,
+                        default=None)
 
     subparsers = parser.add_subparsers(title='subcommands',
                                        description='valid subcommands',
@@ -35,13 +44,34 @@ def main(args=None):
 
     args = vars(parser.parse_args())
     config_file = args['ini_file']
-    env = bootstrap(config_file)
-
+    
     if args['which'] == 'init':
-        init()
+        if os.path.exists(config_file):
+            print("%s already exist." % config_file, file=sys.stderr)
+            sys.exit(1)
+
+        backend = args['backend']
+        if not backend:
+            while True:
+                prompt = ("Which backend to use? "
+                          "(1 - postgresql, 2 - redis, default - memory) ")
+                answer = input(prompt).strip()
+                try:
+                    backends = {"1": "postgresql", "2": "redis", "": "memory"}
+                    backend = backends[answer]
+                    break
+                except KeyError:
+                    pass
         
-    elif args['which'] == 'migrate':
+        init(config_file, backend)
+
+    env = bootstrap(config_file)
+                
+
+    if args['which'] == 'migrate':
+        env = bootstrap(config_file)
         cliquet.init_schema(env)
+
     elif args['which'] == 'start':
         pserve_argv = ['pserve', config_file, '--reload']
         pserve.main(pserve_argv)
--- kinto/config/__init__.py
+++ kinto/config/__init__.py
@@ -5,9 +5,8 @@ from six.moves import input
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-def render_template(template, destination, **kwargs):
-
-
+def render_template(template, destination, values):
+   
    here = os.path.abspath(os.path.dirname(__file__))
    template = os.path.join(here, 'kinto.tpl') 
    destination = os.path.join(here, 'kinto.ini')
@@ -15,42 +14,33 @@ def render_template(template, destination, **kwargs):
    with codecs.open(template, 'r', encoding='utf-8') as f:
         raw_template = f.read()
        
-        rendered = raw_template.format(**kwargs)
+        rendered = raw_template.format(**values)
 
-        with codecs.open(destination, 'w+', encoding='utf-8') as output:
+        with codecs.open(destination, 'w', encoding='utf-8') as output:
             output.write(rendered)
 
+def init(config_file, backend):
+    values = {}
+    values.update({'secret' : binascii.b2a_hex(os.urandom(32)),
+                 'storage_backend' : "cliquet.storage.%s" % backend,
+                 'cache_backend' : "cliquet.cache.%s" % backend,
+                 'permission_backend' : "cliquet.permission.%s" % backend})
 
-def init():
-	values= {}
-	values['secret'] = binascii.b2a_hex(os.urandom(32))
+    if backend == 'postgresql':
+        postgresql_url = "postgres://postgres:postgres@localhost/postgres"
+        values.update({'storage_url' : postgresql_url,
+                     'cache_url' : postgresql_url,
+                     'permission_url' : postgresql_url})
 
-	backend = input("Which backend to use ? (1 - in-memory, 2 - postgresql, 3 - redis) :")
+    elif backend == 'redis':
+        redis_url = "redis://localhost:6379"
+        values.update({'storage_url' : redis_url + "/1",
+                     'cache_url' : redis_url + "/2",
+                     'permission_url' : redis_url + "/3"})
 
-	if backend in ('2', ''):
-	    values['storage_backend'] = "cliquet.storage.postgresql"
-	    values['storage_url'] = "postgres://postgres:postgres@localhost/postgres"
-	    values['cache_backend'] = "cliquet.cache.postgresql"
-	    values['cache_url'] ="postgres://postgres:postgres@localhost/postgres"
-	    values['permission_backend'] = "cliquet.permission.postgresql"
-	    values['permission_url'] = "postgres://postgres:postgres@localhost/postgres"
+    else:
+        values.update({'storage_url' : '',
+                     'cache_url' : '',
+                     'permission_url' : ''})
 
-	if backend == '3':
-	    values['storage_backend'] = "cliquet.storage.redis"
-            values['storage_url'] = "redis://localhos:6379/1"
-	    values['cache_backend'] = "cliquet.cache.redis"
-	    values['cache_url'] = "redis://localhos:6379/2"
-	    values['permission_backend'] = "cliquet.permission.redis"
-	    values['permission_url'] = "redis://localhos:6379/3"
-
-
-	    
-	    
-        render_template("kinto.tpl", "kinto.ini",
-		            secret = values['secret'],
-		            storage_backend = values['storage_backend'],
-		            storage_url = values['storage_url'],
-		            cache_backend = values['cache_backend'],
-		            cache_url = values['cache_url'],
-		            permission_backend = values['permission_backend'],
-		            permission_url = values['permission_url'])
+    render_template("kinto.tpl", config_file, values)
