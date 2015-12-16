5a0e5991be9704ea868a107e7f413a8172f6d3a3
Mathieu Leplatre <mathieu@mozilla.com>
2015-12-16 12:01:38 +0100

    Bump protocol version to 1.1 because of protocol changes in release 1.9

--- kinto/__init__.py
+++ kinto/__init__.py
@@ -14,7 +14,7 @@ from kinto.authorization import RouteFactory
 __version__ = pkg_resources.get_distribution(__package__).version
 
 # Implemented HTTP API Version
-HTTP_API_VERSION = '1.0'
+HTTP_API_VERSION = '1.1'
 
 # Main kinto logger
 logger = logging.getLogger(__name__)
