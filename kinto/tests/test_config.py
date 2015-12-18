7e3c2f7fb53e160d8cabddfd8baf71e13778a10d
oak11 <oaks9599@gmail.com>
2015-12-18 14:24:23 +0530

    add test

---
+++ kinto/tests/test_config.py
@@ -0,0 +1,15 @@
+from kinto.config import render_template, HERE, init
+import os
+import unittest
+
+class ConfigTest(unittest.TestCase):
+
+#to check if values dict is not empty    
+        
+#To check if rendered template generates a file
+   
+    def test_init(self):
+        
+        destination = os.path.join(os.path.abspath(os.path.dirname(__file__)), test.ini)
+        
+        self.assertEqual(init(destination,postgresql),kinto1)
