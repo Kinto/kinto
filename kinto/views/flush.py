6be5eff7cd8648a09784602d3399ecaf18366fbf
John Giannelos <johngiannelos@gmail.com>
2015-12-26 20:01:04 +0200

    Trigger event on server flush.

--- kinto/views/flush.py
+++ kinto/views/flush.py
@@ -2,6 +2,7 @@ from cornice import Service
 from pyramid import httpexceptions
 from pyramid.security import NO_PERMISSION_REQUIRED
 
+from kinto.events import ServerFlushed
 
 flush = Service(name='flush',
                 description='Clear database content',
@@ -13,4 +14,6 @@ def flush_post(request):
     request.registry.storage.flush()
     request.registry.permission.flush()
     request.registry.cache.flush()
+    event = ServerFlushed(request)
+    request.registry.notify(event)
     return httpexceptions.HTTPAccepted()
