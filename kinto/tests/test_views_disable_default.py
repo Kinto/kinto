89af85479820b6b3144ec54a46241a76003042ef
Jelmer van der Ploeg <jelmer@woovar.com>
2016-03-07 10:50:17 -0500

    removed duplicate code

--- kinto/tests/test_views_disable_default.py
+++ kinto/tests/test_views_disable_default.py
@@ -13,29 +13,6 @@ class DisableDefaultBucketViewTest(BaseWebTest, unittest.TestCase):
 
     test_url = '/buckets/default'
 
-    def setUp(self):
-        super(DisableDefaultBucketViewTest, self).setUp()
-        self.events = []
-
-
-    def tearDown(self):
-        self.events = []
-        super(DisableDefaultBucketViewTest, self).tearDown()
-
-    def _get_test_app(self, settings=None):
-        app_settings = self.get_app_settings(settings)
-        self.config = Configurator(settings=app_settings)
-        app = webtest.TestApp(main({}, config=self.config, **app_settings))
-        app.RequestClass = cliquet_support.get_request_class(prefix="v1")
-
-        return app
-
-    def get_app_settings(self, extra=None):
-        if extra is None:
-            extra = {}
-        settings = super(DisableDefaultBucketViewTest, self).get_app_settings(extra)
-        return settings
-
     def test_returns_403_if_excluded_in_configuration(self):
         extra = {'includes': ''}
         app = self._get_test_app(settings=extra)
