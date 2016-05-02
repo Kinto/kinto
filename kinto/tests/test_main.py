cb47b341b04ba8df8cdedc6a34c2c4e4fcfdd61d
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-05-02 16:28:04 +0200

    @leplatrem review.

--- kinto/tests/test_main.py
+++ kinto/tests/test_main.py
@@ -9,108 +9,111 @@ import mock
 import os
 import pytest
 import sys
+import tempfile
 import unittest
 
 from six import StringIO
 
 from kinto.__main__ import main
 
+_, TEMP_KINTO_INI = tempfile.mkstemp(prefix='kinto_config')
+
 
 class TestMain(unittest.TestCase):
-    def tearDown(self):
+    def setUp(self):
         try:
-            os.remove('/tmp/kinto.ini')
+            os.remove(TEMP_KINTO_INI)
         except OSError:
             pass
 
-    def test_cli_init_generate_configuration(self):
-        res = main(['--ini', '/tmp/kinto.ini', '--backend', 'memory', 'init'])
+    def test_cli_init_generates_configuration(self):
+        res = main(['--ini', TEMP_KINTO_INI, '--backend', 'memory', 'init'])
         assert res == 0
-        assert os.path.exists('/tmp/kinto.ini')
+        assert os.path.exists(TEMP_KINTO_INI)
 
     def test_cli_init_returns_if_file_exists(self):
-        with open('/tmp/kinto.ini', 'w') as f:
+        with open(TEMP_KINTO_INI, 'w') as f:
             f.write("exists")
         with mock.patch('sys.stderr', new_callable=StringIO) as mock_stderr:
-            res = main(['--ini', '/tmp/kinto.ini',
+            res = main(['--ini', TEMP_KINTO_INI,
                         '--backend', 'memory', 'init'])
             assert res == 1
             content = mock_stderr.getvalue()
-            assert '/tmp/kinto.ini already exists.' in content
+            assert '{} already exists.'.format(TEMP_KINTO_INI) in content
 
-    def test_cli_init_ask_for_backend_if_not_specified(self):
+    def test_cli_init_asks_for_backend_if_not_specified(self):
         with mock.patch("kinto.__main__.input", create=True, return_value="2"):
-            res = main(['--ini', '/tmp/kinto.ini', 'init'])
+            res = main(['--ini', TEMP_KINTO_INI, 'init'])
             assert res == 0
 
-    def test_cli_init_ask_until_backend_is_valid(self):
+    def test_cli_init_asks_until_backend_is_valid(self):
         with mock.patch("kinto.__main__.input", create=True,
                         side_effect=["10", "2"]):
-            res = main(['--ini', '/tmp/kinto.ini', 'init'])
+            res = main(['--ini', TEMP_KINTO_INI, 'init'])
             assert res == 0
-            with open('/tmp/kinto.ini') as f:
+            with open(TEMP_KINTO_INI) as f:
                 content = f.read()
             assert 'redis' in content
 
     def test_fails_if_not_enough_args(self):
         with mock.patch('sys.stderr', new_callable=StringIO) as mock_stderr:
             with pytest.raises(SystemExit) as excinfo:
-                main(['--ini', '/tmp/kinto.ini'])
+                main(['--ini', TEMP_KINTO_INI])
             assert 'INI_FILE' in mock_stderr.getvalue()
             assert excinfo.value.code == 2
 
-    def test_cli_init_install_postgresql_dependencies_if_needed(self):
+    def test_cli_init_installs_postgresql_dependencies_if_needed(self):
         realimport = builtins.__import__
 
-        def myimport(name, *args, **kwargs):
+        def psycopg2_missing(name, *args, **kwargs):
             if name == 'psycopg2':
                 raise ImportError()
             else:
                 return realimport(name, *args, **kwargs)
 
         with mock.patch('{}.__import__'.format(builtins_name),
-                        side_effect=myimport):
+                        side_effect=psycopg2_missing):
             with mock.patch('pip.main', return_value=None) as mocked_pip:
                 with mock.patch("kinto.__main__.input", create=True,
                                 return_value="1"):
-                    res = main(['--ini', '/tmp/kinto.ini', 'init'])
+                    res = main(['--ini', TEMP_KINTO_INI, 'init'])
                     assert res == 0
                     assert mocked_pip.call_count == 1
 
     def test_main_takes_sys_argv_by_default(self):
-        testargs = ["prog", "--ini", "/tmp/kinto.ini",
+        testargs = ["prog", "--ini", TEMP_KINTO_INI,
                     '--backend', 'memory', 'init']
         with mock.patch.object(sys, 'argv', testargs):
             main()
 
-        with open('/tmp/kinto.ini') as f:
+        with open(TEMP_KINTO_INI) as f:
             content = f.read()
         assert 'memory' in content
 
     def test_cli_migrate_command_runs_init_schema(self):
         with mock.patch('kinto.__main__.cliquet.init_schema') as mocked_init:
-            res = main(['--ini', '/tmp/kinto.ini',
+            res = main(['--ini', TEMP_KINTO_INI,
                         '--backend', 'memory', 'init'])
             assert res == 0
-            res = main(['--ini', '/tmp/kinto.ini', 'migrate'])
+            res = main(['--ini', TEMP_KINTO_INI, 'migrate'])
             assert res == 0
             assert mocked_init.call_count == 1
 
     def test_cli_start_runs_pserve(self):
         with mock.patch('kinto.__main__.pserve.main') as mocked_pserve:
-            res = main(['--ini', '/tmp/kinto.ini',
+            res = main(['--ini', TEMP_KINTO_INI,
                         '--backend', 'memory', 'init'])
             assert res == 0
-            res = main(['--ini', '/tmp/kinto.ini', 'start'])
+            res = main(['--ini', TEMP_KINTO_INI, 'start'])
             assert res == 0
             assert mocked_pserve.call_count == 1
 
     def test_cli_start_with_reload_runs_pserve_with_reload(self):
         with mock.patch('kinto.__main__.pserve.main') as mocked_pserve:
-            res = main(['--ini', '/tmp/kinto.ini',
+            res = main(['--ini', TEMP_KINTO_INI,
                         '--backend', 'memory', 'init'])
             assert res == 0
-            res = main(['--ini', '/tmp/kinto.ini', 'start', '--reload'])
+            res = main(['--ini', TEMP_KINTO_INI, 'start', '--reload'])
             assert res == 0
             assert mocked_pserve.call_count == 1
             assert '--reload' in mocked_pserve.call_args[0][0]
