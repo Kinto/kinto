a38a53c9122af5c2081d93f7738fb199cbdc0480
Mathieu Leplatre <mathieu@mozilla.com>
2016-05-25 17:46:59 +0200

    Query every perms

--- kinto/views/permissions.py
+++ kinto/views/permissions.py
@@ -3,6 +3,8 @@ from pyramid.request import Request
 from pyramid.interfaces import IRoutesMapper
 from pyramid.security import NO_PERMISSION_REQUIRED, Authenticated
 
+from kinto.authorization import PERMISSIONS_INHERITANCE_TREE
+
 
 permissions = Service(name='permissions',
                       description='List of user permissions',
@@ -12,8 +14,19 @@ permissions = Service(name='permissions',
 @permissions.get(permission=NO_PERMISSION_REQUIRED)
 def permissions_get(request):
     backend = request.registry.permission
-    # XXX : add setting «experimental» ?
+    # XXX : add setting experimental?
     # XXX : if backend is None, route should be ignored in kinto init
+
+    possible_perms = set([k.split(':', 1)[1]
+                          for k in PERMISSIONS_INHERITANCE_TREE.keys()])
+
+    perms_descending_tree = {}
+    for obtained_perm, obtained_from in PERMISSIONS_INHERITANCE_TREE.items():
+        for resource, perms in obtained_from.items():
+            for perm in perms:
+                perms_descending_tree.setdefault(resource, {})\
+                                     .setdefault(perm, set())\
+                                     .add(obtained_perm)
 
     # Obtain current principals.
     principals = request.effective_principals
@@ -22,34 +35,37 @@ def permissions_get(request):
         userid = request.prefixed_userid
         principals.append(userid)
 
-    # XXX should work with read and others
-    object_uris = backend.principals_accessible_objects(principals, 'write')
+    perms_by_object_uri = {}
+    for perm in possible_perms:
+        object_uris = backend.principals_accessible_objects(principals, perm)
+        for object_uri in object_uris:
+            perms_by_object_uri.setdefault(object_uri, []).append(perm)
 
+    api_prefix = '/%s' % request.upath_info.split('/')[1]
     q = request.registry.queryUtility
     routes_mapper = q(IRoutesMapper)
 
     entries = []
-    for object_uri in object_uris:
+    for object_uri, perms in perms_by_object_uri.items():
         # Obtain associated resource to object URI
         # XXX: move to core.utils
-        # XXX: hard coded v1
-        fakerequest = Request.blank(path='/v1' + object_uri)
+        fakerequest = Request.blank(path=api_prefix + object_uri)
         info = routes_mapper(fakerequest)
         match, route = info['match'], info['route']
-        resource_name = route.name.replace('-record', '')
+        resource_name = route.name.replace('-record', '')\
+                                  .replace('-collection', '')
         match['%s_id' % resource_name] = match.pop('id')
 
-        # XXX hard coded perms tree
-        # XXX: move to helper in authorization
-        permissions = ['read', 'write']
-        if resource_name == 'bucket':
-            permissions.append('collection:create')
-        elif resource_name == 'collection':
-            permissions.append('record:create')
+        permissions = set(perms)
+        for perm in perms:
+            obtained = perms_descending_tree[resource_name][perm]
+            shown = [p.split(':', 1)[1]
+                     for p in obtained if p.startswith(resource_name)]
+            permissions |= set(shown)
 
         entry = dict(uri=object_uri,
                      resource_name=resource_name,
-                     permissions=permissions,
+                     permissions=list(permissions),
                      **match)
         entries.append(entry)
 
