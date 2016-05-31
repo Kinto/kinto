5afdbba542285c66baf4422a1968237c063e2fe0
Mathieu Leplatre <mathieu@mozilla.com>
2016-05-31 12:08:51 +0200

    Preparing release 3.1.1

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ ENTRY_POINTS = {
 
 
 setup(name='kinto',
-      version='3.1.0',
+      version='3.1.1',
       description='Kinto Web Service - Store, Sync, Share, and Self-Host.',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
