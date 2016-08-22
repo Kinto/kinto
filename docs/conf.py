2ec24759ee9658919cf32e92aa0c965ecba5bea6
Mathieu Leplatre <mathieu@mozilla.com>
2016-08-22 17:45:26 +0200

    Preparing release 4.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -72,9 +72,9 @@ copyright = u'2015-2016 — Mozilla Services'
 # built documents.
 #
 # The short X.Y version.
-version = '4.0'
+version = '4.1'
 # The full version, including alpha/beta/rc tags.
-release = '4.0.1'
+release = '4.1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
