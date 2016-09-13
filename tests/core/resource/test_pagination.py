75999a77e1e466e787cb537d4080ff4c2a892a63
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-09-13 15:00:41 +0200

    Add a test.
    @leplatrem review.

--- tests/core/resource/test_pagination.py
+++ tests/core/resource/test_pagination.py
@@ -23,6 +23,8 @@ class PaginationTest(BaseTest):
                 'status': i % 4,
                 'unread': (i % 2 == 0)
             }
+            if i % 3 == 0:
+                record['optional'] = True
             self.model.create_record(record)
 
     def _setup_next_page(self):
@@ -193,6 +195,15 @@ class PaginationTest(BaseTest):
             '_since': '123', '_limit': '20',
             '_token': b64encode(invalid_token.encode('ascii')).decode('ascii')}
         self.assertRaises(HTTPBadRequest, self.resource.collection_get)
+
+    def test_next_page_url_works_with_optional_fields(self):
+        self.resource.request.GET = {'_limit': '10', '_sort': '-optional'}
+        results1 = self.resource.collection_get()
+        self._setup_next_page()
+        results2 = self.resource.collection_get()
+        results_id1 = set([x['id'] for x in results1['data']])
+        results_id2 = set([x['id'] for x in results2['data']])
+        self.assertFalse(results_id1.intersection(results_id2))
 
 
 class BuildPaginationTokenTest(BaseTest):
