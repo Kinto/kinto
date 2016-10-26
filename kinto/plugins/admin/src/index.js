0fd28ebd690d616bda179d2ce78ff3744773df14
Nicolas Perriault <nicolas@perriault.net>
2016-10-26 18:34:08 +0200

    Revamp kinto-admin install/setup/upgrade procedure.

--- kinto/plugins/admin/src/index.js
+++ kinto/plugins/admin/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import KintoAdmin from "kinto-admin";
 import * as signoffPlugin from "kinto-admin/lib/plugins/signoff";
 
+
 ReactDOM.render(
   <KintoAdmin settings={{maxPerPage: 50}} plugins={[signoffPlugin]}/>,
   document.getElementById("root")
