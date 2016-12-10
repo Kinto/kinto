a4cf5eb4d0368e5adb942041761fb6300d5d9dcf
Gabriela Surita <gabsurita@gmail.com>
2016-12-10 18:43:13 -0200

    dummy fix memory issues

--- tests/swagger/support.py
+++ tests/swagger/support.py
@@ -1,4 +1,3 @@
-
 import unittest
 
 from bravado_core.spec import Spec
@@ -9,16 +8,22 @@ from ..support import (BaseWebTest, MINIMALIST_BUCKET, MINIMALIST_GROUP,
                        MINIMALIST_COLLECTION, MINIMALIST_RECORD)
 
 
+app = BaseWebTest().make_app()
+
+spec_dict = app.get('/swagger.json').json
+spec = Spec.from_dict(spec_dict)
+resources = build_resources(spec)
+
+
 class SwaggerTest(BaseWebTest, unittest.TestCase):
 
-    def __init__(self, *args, **kargs):
-        super(SwaggerTest, self).__init__(*args, **kargs)
-
-        self.spec_dict = self.app.get('/swagger.json').json
-        self.spec = Spec.from_dict(self.spec_dict)
-        self.resources = build_resources(self.spec)
-
     def setUp(self):
+
+        # FIXME: solve memory issues from generating the spec multiple times
+        self.spec_dict = spec_dict
+        self.spec = spec
+        self.resources = resources
+
         super(SwaggerTest, self).setUp()
 
         self.bucket = self.app.put_json('/buckets/b1',
@@ -40,12 +45,13 @@ class SwaggerTest(BaseWebTest, unittest.TestCase):
     def cast_bravado_response(self, response):
         resp = OutgoingResponse()
         resp.text = response.body
-        # XXX: empty response fails with Python 3
+        # FIXME: Empty response fails with Bravado and Python 3
         # See https://github.com/Yelp/bravado-core/issues/134
         if not resp.text:
             resp.text = None
         resp.headers = response.headers
-        # XXX: Drop charset (might be a bug on bravado)
+        # FIXME: Drop charset from application/json response on Pyramid
+        # See https://github.com/Pylons/pyramid/issues/2860
         resp.content_type = response.headers.get('Content-Type', '').split(';')[0]
         resp.json = lambda: response.json
 
