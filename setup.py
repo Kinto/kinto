083a145089f1f5b0274143e23934e0107804c77a
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-02-16 16:20:47 +0100

    Back to development. 6.0.0

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ ENTRY_POINTS = {
 
 
 setup(name='kinto',
-      version='5.4.0.dev0',
+      version='6.0.0.dev0',
       description='Kinto Web Service - Store, Sync, Share, and Self-Host.',
       long_description="{}\n\n{}\n\n{}".format(README, CHANGELOG, CONTRIBUTORS),
       license='Apache License (2.0)',
