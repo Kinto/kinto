32e5e1b838dbb04100c6f77a289d810694929964
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-05-25 11:32:47 -0400

    Back to development: 6.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ Changelog
 
 This document describes changes between each past release.
 
+6.1.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 6.1.0 (2017-05-25)
 ------------------
 
--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ ENTRY_POINTS = {
 
 
 setup(name='kinto',
-      version='6.1.0',
+      version='6.1.1.dev0',
       description='Kinto Web Service - Store, Sync, Share, and Self-Host.',
       long_description="{}\n\n{}\n\n{}".format(README, CHANGELOG, CONTRIBUTORS),
       license='Apache License (2.0)',
