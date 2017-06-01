fdc5bed4928037233ce8dbdbbf31390eb47268a2
Mathieu Leplatre <mathieu@mozilla.com>
2017-06-01 11:34:38 +0200

    @Natim review

--- tests/plugins/test_accounts.py
+++ tests/plugins/test_accounts.py
@@ -235,3 +235,10 @@ class AdminTest(AccountsWebTest):
 
         resp = self.app.get('/accounts/alice', headers=get_user_headers('alice', 'bouh'))
         assert resp.json['permissions'] == {'write': ['account:alice']}
+
+    def test_user_created_by_admin_can_delete_her_record(self):
+        self.app.post_json('/accounts',
+                           {'data': {'id': 'alice', 'password': 'bouh'}},
+                           headers=self.admin_headers)
+
+        self.app.delete('/accounts/alice', headers=get_user_headers('alice', 'bouh'))
