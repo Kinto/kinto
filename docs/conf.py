434cd87fbdb9ceed24e34067c2e7ab22e463dd6e
Mathieu Leplatre <mathieu@mozilla.com>
2018-04-06 13:51:39 +0200

    @leplatrem review

--- docs/conf.py
+++ docs/conf.py
@@ -73,9 +73,9 @@ copyright = '2015-2017 — Mozilla Services'
 # built documents.
 #
 # The short X.Y version.
-version = '8.2'
+version = '8.3'
 # The full version, including alpha/beta/rc tags.
-release = '8.2.3'
+release = '8.3.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
