9d7a6920c3b0ea3050b45bbc31437a9df1bafe41
Mathieu Leplatre <mathieu@mozilla.com>
2018-05-28 23:00:00 +0200

    Clean-up unused globalSettings in admin plugin

--- kinto/plugins/admin/src/index.js
+++ kinto/plugins/admin/src/index.js
@@ -6,7 +6,6 @@ import * as signoffPlugin from "kinto-admin/lib/plugins/signoff";
 const settings = {
   maxPerPage: 50,
   singleServer: document.location.toString().split('/admin/')[0],
-  ...window.globalSettings,
 };
 
 
