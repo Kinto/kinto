405724d6572a5a9cc07c31a3c6a933bdb2704ae4
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-01-03 15:54:50 -0500

    Add tests for event emission

--- tests/test_views_admin.py
+++ tests/test_views_admin.py
@@ -1,5 +1,7 @@
+from pyramid.config import Configurator
 import unittest
 
+from kinto.core import events
 from kinto.core.storage import exceptions
 from kinto.core.testing import get_user_headers
 from kinto.core import utils
@@ -26,6 +28,12 @@ class DeleteUserDataTest(BaseWebTest, unittest.TestCase):
     doomed_hash = utils.hmac_digest(hmac_secret, "doomed:trustno1")
     doomed_user_principal = f"basicauth:{doomed_hash}"
     delete_user_url = f"/__user_data__/{doomed_user_principal}"
+    subscribed = (events.ResourceChanged,)
+    events = []
+
+    @classmethod
+    def listener(cls, event):
+        cls.events.append(event)
 
     @classmethod
     def get_app_settings(cls, extras=None):
@@ -35,7 +43,17 @@ class DeleteUserDataTest(BaseWebTest, unittest.TestCase):
         dct["bucket_create_principals"] = "system.Authenticated"
         return dct
 
+    @classmethod
+    def make_app(cls, settings=None, config=None):
+        settings = cls.get_app_settings(settings)
+        config = Configurator(settings=settings)
+        for event_cls in cls.subscribed:
+            config.add_subscriber(cls.listener, event_cls)
+        config.commit()
+        return super().make_app(settings=settings, config=config)
+
     def setUp(self):
+        del self.events[:]
         super().setUp()
         self.app.put_json(self.doomed_bucket_url, MINIMALIST_BUCKET, headers=self.doomed_user)
         self.app.put_json(
@@ -167,6 +185,49 @@ class DeleteUserDataTest(BaseWebTest, unittest.TestCase):
     def test_cannot_delete_user_without_permission(self):
         # Only the user with self.headers can access this URL.
         self.app.delete(self.delete_user_url, headers=self.doomed_user, status=403)
+
+    def test_event_emitted_for_bucket_deletion(self):
+        delete_events = [
+            e for e in self.events if e.payload["action"] == events.ACTIONS.DELETE.value
+        ]
+        bucket_events = [e for e in delete_events if e.payload["resource_name"] == "bucket"]
+        # There should only be one such event, because all buckets
+        # share a parent
+        self.assertEqual(len(bucket_events), 1)
+        # There should only be one such bucket
+        self.assertEqual(len(bucket_events[0].impacted_objects), 1)
+        event = bucket_events[0].impacted_objects[0]
+        self.assertEqual(event["old"]["id"], "user_to_delete")
+
+    def test_event_not_emitted_for_coalesced_collection_deletion(self):
+        # Verify that collection deletion for
+        # /buckets/user_to_delete/collections/collection_to_delete
+        # was folded into bucket deletion for
+        # /buckets/user_to_delete
+        delete_events = [
+            e for e in self.events if e.payload["action"] == events.ACTIONS.DELETE.value
+        ]
+        collection_events = [
+            e for e in delete_events if e.payload["resource_name"] == "collection"
+        ]
+        # There should only be one such event, for /buckets/user_safe
+        self.assertEqual(len(collection_events), 1)
+        self.assertEqual(collection_events[0].payload["bucket_id"], "user_safe")
+        # Nothing for user_to_delete, so it must have been coalesced
+
+    def test_event_emitted_for_record_deletion(self):
+        delete_events = [
+            e for e in self.events if e.payload["action"] == events.ACTIONS.DELETE.value
+        ]
+        record_events = [e for e in delete_events if e.payload["resource_name"] == "record"]
+        # Just one, for safe_collection_doomed_record_url
+        self.assertEqual(len(record_events), 1)
+        event = record_events[0]
+        self.assertEqual(event.payload["bucket_id"], "user_safe")
+        self.assertEqual(event.payload["collection_id"], "collection_safe")
+        # Just the doomed record, not the safe one
+        self.assertEqual(len(event.impacted_objects), 1)
+        self.assertEqual(event.impacted_objects[0]["old"]["id"], "record_to_delete")
 
 
 class GetParentUriTest(unittest.TestCase):
