8d2a59ff813734d86a56b2218f39bb43285d98eb
Rémy HUBSCHER <remy@chefclub.tv>
2019-01-08 15:46:08 +0100

    Fix tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -364,6 +364,19 @@ class TestMain(unittest.TestCase):
             )
 
     def test_cli_flush_cache_command_runs_flush_cache_script(self):
+        # Build a temporary ini file.
+        res = main(
+            [
+                "init",
+                "--ini",
+                TEMP_KINTO_INI,
+                "--backend",
+                "memory",
+                "--cache-backend",
+                "memory",
+            ]
+        )
+        assert res == 0
         with mock.patch("kinto.__main__.core_scripts.flush_cache") as mocked_cache_script:
             res = main(["flush-cache", "--ini", TEMP_KINTO_INI])
             assert res == 0
