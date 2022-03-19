687cb1e35d3d74f9a6c994b88cfd635eb502df92
Graham Beckley <gbeckley@mozilla.com>
2022-03-19 12:32:01 -0400

    @leplatrem feedback
    - add v to tag variable
    - add new VERSION variable
    - use pushd / popd for building bundle
    - add TARGET_DIR variable

--- scripts/build-kinto-admin.sh
+++ scripts/build-kinto-admin.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-
-TAG=$(cat kinto/plugins/admin/VERSION)
-SRC_DIR="kinto-admin-${TAG}"
-TARBALL_NAME="${SRC_DIR}.tar.gz"
+VERSION=$(cat kinto/plugins/admin/VERSION)
+TAG="v${VERSION}"
+TARBALL_NAME="${TAG}.tar.gz"
+SRC_DIR="kinto-admin-${VERSION}"
 
 # download and unzip release
 # TODO: error handling (404 error, can't unzip tarball, etc)
 cd kinto/plugins/admin/
-curl -L https://github.com/Kinto/kinto-admin/archive/refs/tags/v${TAG}.tar.gz --output $TARBALL_NAME
+curl -OL https://github.com/Kinto/kinto-admin/archive/refs/tags/${TAG}.tar.gz
 tar -xf $TARBALL_NAME && rm $TARBALL_NAME
 
 # build kinto-admin bundle
-cd $SRC_DIR
+pushd $SRC_DIR
 npm ci
 export SINGLE_SERVER=1
 npm run build
+popd
 
 # move build and delete source
-rm -rf ../build
-mv -f build ../
-cd ../ && rm -rf $SRC_DIR
+TARGET_DIR=./build
+rm -rf $TARGET_DIR
+mv $SRC_DIR/build $TARGET_DIR
+rm -rf $SRC_DIR
