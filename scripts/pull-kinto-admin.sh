e67d65907c502e753a995aadc677dd9c231db41b
Alex Cottner <acottner@mozilla.com>
2024-02-02 10:25:17 -0700

    Adjustements based on PR feedback

--- scripts/pull-kinto-admin.sh
+++ scripts/pull-kinto-admin.sh
@@ -9,4 +9,4 @@ curl -OL "https://github.com/Kinto/kinto-admin/releases/download/${TAG}/kinto-ad
 rm -r ./kinto/plugins/admin/build || echo "admin/build folder doesn't exist yet"
 mkdir ./kinto/plugins/admin/build
 tar -xf kinto-admin-release.tar -C ./kinto/plugins/admin/build && rm kinto-admin-release.tar
-echo "$VERSION" > ./kinto/plugins/admin/build/VERSION
+echo "$VERSION" > ./kinto/plugins/admin/build/VERSION # will not be needed after kinto-admin@8400176 (version 3.0.4?)
