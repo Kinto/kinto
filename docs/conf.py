321f61bc06503cb10d47d5de3e799940261f4068
Mathieu Leplatre <mathieu@mozilla.com>
2024-10-16 17:40:28 +0200

    Do not specify theme path

--- docs/conf.py
+++ docs/conf.py
@@ -24,10 +24,7 @@ __HERE__ = os.path.dirname(os.path.abspath(__file__))
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
     html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.io uses their theme by default, so no need to specify
 # it
