64199b5f46706e7916e5e2d3c02717255afab6a0
SovunhVoeu <svoeu@student.bridgew.edu>
2025-04-12 09:53:47 -0400

    Issue #1621
    Testing the precision accuracy

--- kinto/core/storage/postgresql/schema.sql
+++ kinto/core/storage/postgresql/schema.sql
@@ -1,54 +1,37 @@
---
 -- Automated script, we do not need NOTICE and WARNING
---
 SET client_min_messages TO ERROR;
---
--- Convert timestamps to milliseconds epoch integer
---
-CREATE OR REPLACE FUNCTION as_epoch(ts TIMESTAMP) RETURNS BIGINT AS $$
+
+-- Convert timestamps to microseconds epoch integer
+CREATE OR REPLACE FUNCTION as_epoch_micro(ts TIMESTAMP) RETURNS NUMERIC AS $$
 BEGIN
-    RETURN (EXTRACT(EPOCH FROM ts) * 1000)::BIGINT;
+    RETURN EXTRACT(EPOCH FROM ts) * 1000000;
 END;
-$$ LANGUAGE plpgsql
-IMMUTABLE;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION from_epoch(epoch BIGINT) RETURNS TIMESTAMP AS $$
 BEGIN
     RETURN TIMESTAMP WITH TIME ZONE 'epoch' + epoch * INTERVAL '1 millisecond';
 END;
-$$ LANGUAGE plpgsql
-IMMUTABLE;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
---
 -- Actual objects
---
 CREATE TABLE IF NOT EXISTS objects (
-    -- These are all IDs stored as text, and not human language.
-    -- Therefore, we store them in the C collation. This lets Postgres
-    -- use the index on parent_id for prefix matching (parent_id LIKE
-    -- '/buckets/abc/%').
     id TEXT COLLATE "C" NOT NULL,
     parent_id TEXT COLLATE "C" NOT NULL,
     resource_name TEXT COLLATE "C" NOT NULL,
-
-    -- Timestamp is relevant because adequate semantically.
-    -- Since the HTTP API manipulates integers, it could make sense
-    -- to replace the timestamp columns type by integer.
     last_modified TIMESTAMP NOT NULL,
-
-    -- JSONB, 2x faster than JSON.
     data JSONB NOT NULL DEFAULT '{}'::JSONB,
-
     deleted BOOLEAN NOT NULL,
-
     PRIMARY KEY (id, parent_id, resource_name)
 );
+
+-- Update the index to use as_epoch_micro for better precision
 CREATE UNIQUE INDEX IF NOT EXISTS idx_objects_parent_id_resource_name_last_modified
     ON objects(parent_id, resource_name, last_modified DESC);
-CREATE INDEX IF NOT EXISTS idx_objects_last_modified_epoch
-    ON objects(as_epoch(last_modified));
+CREATE INDEX IF NOT EXISTS idx_objects_last_modified_epoch_micro
+    ON objects(as_epoch_micro(last_modified));
 
-
+-- Timestamps table
 CREATE TABLE IF NOT EXISTS timestamps (
   parent_id TEXT NOT NULL COLLATE "C",
   resource_name TEXT NOT NULL COLLATE "C",
@@ -56,9 +39,7 @@ CREATE TABLE IF NOT EXISTS timestamps (
   PRIMARY KEY (parent_id, resource_name)
 );
 
---
 -- Triggers to set last_modified on INSERT/UPDATE
---
 DROP TRIGGER IF EXISTS tgr_objects_last_modified ON objects;
 
 CREATE OR REPLACE FUNCTION bump_timestamp()
@@ -75,7 +56,7 @@ BEGIN
         FROM objects
         WHERE parent_id = NEW.parent_id
           AND resource_name = NEW.resource_name
-        ORDER BY as_epoch(last_modified) DESC
+        ORDER BY as_epoch_micro(last_modified) DESC
         LIMIT 1
       )
       -- Timestamp when resource was empty.
@@ -87,27 +68,18 @@ BEGIN
           AND resource_name = NEW.resource_name
       )
     )
-    SELECT as_epoch(MAX(last_modified)) INTO previous
+    SELECT as_epoch_micro(MAX(last_modified)) INTO previous
       FROM existing_timestamps;
 
-    --
-    -- This bumps the current timestamp to 1 msec in the future if the previous
-    -- timestamp is equal to the current one (or higher if was bumped already).
-    --
-    -- If a bunch of requests from the same user on the same resource
-    -- arrive in the same millisecond, the unicity constraint can raise
-    -- an error (operation is cancelled).
-    -- See https://github.com/mozilla-services/cliquet/issues/25
-    --
-    current := as_epoch(clock_timestamp()::TIMESTAMP);
+    -- This bumps the current timestamp to 1 msec in the future if the previous timestamp is equal to the current one.
+    current := as_epoch_micro(clock_timestamp()::TIMESTAMP);
     IF previous IS NOT NULL AND previous >= current THEN
         current := previous + 1;
     END IF;
 
     IF NEW.last_modified IS NULL OR
-       (previous IS NOT NULL AND as_epoch(NEW.last_modified) = previous) THEN
-        -- If record does not carry last-modified, or if the one specified
-        -- is equal to previous, assign it to current (i.e. bump it).
+       (previous IS NOT NULL AND as_epoch_micro(NEW.last_modified) = previous) THEN
+        -- If record does not carry last-modified, or if the one specified is equal to previous, assign it to current (i.e. bump it).
         NEW.last_modified := from_epoch(current);
     END IF;
 
@@ -119,16 +91,12 @@ CREATE TRIGGER tgr_objects_last_modified
 BEFORE INSERT OR UPDATE OF data ON objects
 FOR EACH ROW EXECUTE PROCEDURE bump_timestamp();
 
---
 -- Metadata table
---
 CREATE TABLE IF NOT EXISTS metadata (
     name VARCHAR(128) NOT NULL,
     value VARCHAR(512) NOT NULL
 );
 INSERT INTO metadata (name, value) VALUES ('created_at', NOW()::TEXT);
 
-
 -- Set storage schema version.
--- Should match ``kinto.core.storage.postgresql.PostgreSQL.schema_version``
 INSERT INTO metadata (name, value) VALUES ('storage_schema_version', '22');
