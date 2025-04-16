9ffa889659694812a79ed7d2afb038f7589ac6ac
ThomasRoch18 <thomascroch18@gmail.com>
2025-04-16 02:23:10 -0400

    Removed duplicated line in test_the_timestamp_are_based_on_real_time_milliseconds

--- kinto/core/storage/testing.py
+++ kinto/core/storage/testing.py
@@ -819,13 +819,11 @@ class TimestampsTest:
         time.sleep(0.002)  # 2 msec
         obj = self.create_object()
         now = int(obj["last_modified"])
-        now = int(obj["last_modified"])
         if now > 1e15:  # microseconds
             now //= 1000
         elif now < 1e12:  # seconds
             now *= 1000
         # now is in milliseconds
-
         time.sleep(0.002)  # 2 msec
         after = utils.msec_time()
         self.assertTrue(before < now < after, f"{before} < {now} < {after}")
