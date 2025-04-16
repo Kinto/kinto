35138cbe4e1ac3f268f82ccefefd8b620934cd09
ThomasRoch18 <thomascroch18@gmail.com>
2025-04-16 00:45:25 -0400

    kept int function on 'now', reverted before/after variables

--- kinto/core/storage/testing.py
+++ kinto/core/storage/testing.py
@@ -815,12 +815,12 @@ class TimestampsTest:
         self.assertEqual(first, second)
 
     def test_the_timestamp_are_based_on_real_time_milliseconds(self):
-        before = utils.msec_time() * 1000
+        before = utils.msec_time()
         time.sleep(0.002)  # 2 msec
         obj = self.create_object()
         now = int(obj["last_modified"])
         time.sleep(0.002)  # 2 msec
-        after = utils.msec_time() * 1000
+        after = utils.msec_time()
         self.assertTrue(before < now < after, f"{before} < {now} < {after}")
 
     def test_timestamp_are_always_incremented_above_existing_value(self):
