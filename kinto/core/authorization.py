46fbf2a3a36b47fd8f833c146ca0482ef897d8c0
FrancoMorales1 <francomorales0001@gmail.com>
2026-04-22 13:57:29 -0300

    Fix HTTP 500 when object ID contains regex special chars
    Escape special regex characters in object IDs before passing them
    to get_accessible_objects. The '*' wildcard used by plural endpoints
    is preserved after escaping.
    
    Fixes #2676

--- kinto/core/authorization.py
+++ kinto/core/authorization.py
@@ -1,5 +1,6 @@
 import functools
 import logging
+import re
 
 from pyramid.authorization import Authenticated
 from pyramid.interfaces import IAuthorizationPolicy
@@ -228,7 +229,13 @@ class RouteFactory:
             bound_perms = get_bound_permissions(self._object_id_match, perm)
         else:
             bound_perms = [(self._object_id_match, perm)]
-        by_obj_id = self._get_accessible_objects(principals, bound_perms, with_children=False)
+        
+        safe_bound_perms = [
+            (re.escape(obj_id).replace(r'\*', '*') if obj_id else obj_id, p)
+            for obj_id, p in bound_perms
+        ]
+
+        by_obj_id = self._get_accessible_objects(principals, safe_bound_perms, with_children=False)
         ids = by_obj_id.keys()
         # Store for later use in ``Resource``.
         self.shared_ids = [self._extract_object_id(id_) for id_ in ids]
